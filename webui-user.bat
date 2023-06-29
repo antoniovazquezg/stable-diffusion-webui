4e603cdd7773d4bac2f352e10a63a071d3ba5c4f
antoniovazquezg <antoniovazquezg@gmail.com>
2023-06-29 10:28:55 +0200

    SD Parametrs as an acordion for mobile devices

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,10 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--xformers --listen --api
+set DREAMBOOTH_SKIP_INSTALL=True
+set SAFETENSORS_FAST_GPU=1
+
+rem call update.bat
 
 call webui.bat
